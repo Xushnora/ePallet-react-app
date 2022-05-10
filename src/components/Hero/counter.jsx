ee71b5f41d1f7bdc0dea174a7b1af8af6bd9d1e8
Xushnora <xushnoranishonova@gmail.com>
2022-05-09 23:26:55 -0700

    changed counter

--- src/components/Hero/counter.jsx
+++ src/components/Hero/counter.jsx
@@ -5,12 +5,22 @@ class Counter extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 0
+            count: 0,
+            result: 32,
+            resultPit: 60
         }
     }
 
     handlePlus = () => {
         this.setState({count: this.state.count + 1})
+        this.setState({result: this.state.result + 32})
+        this.setState({resultPit: this.state.resultPit + 60})
+    }
+
+    handleMinus = () => {
+        this.setState({count: this.state.count - 1})
+        this.setState({result: this.state.result - 32})
+        this.setState({resultPit: this.state.resultPit - 60})
     }
 
     render() {
@@ -28,19 +38,21 @@ class Counter extends Component {
                     </a>
                     <div className="sumBox">
                         <div className="price1">
-                            <p className="sum">$32</p>
+                            <p className="sum">${this.state.result}</p>
                             <span className="valuta">/Cs</span>
                             <span>"[$0.21/oz]"</span>
                         </div>
                         <div className="price1">
-                            <p className="sum">$730</p>
+                            <p className="sum">${this.state.resultPit}</p>
                             <span className="valuta">/Pit</span>
                         </div>
                     </div>
                     <p className="infoText">Apprux Lead Time: 12 days</p>
-                    <div>
+                    <div className="priceBtnGr">
                        <div className="priceInput">
                             <input type="number" />
+                            <button onClick={this.handlePlus}>+</button>
+                            <button onClick={this.handleMinus}>-</button>
                        </div>
                        <div className="priceBtnBox">
                            <button className="cardsBtn">
@@ -52,7 +64,7 @@ class Counter extends Component {
                                <span>Custom Pallet</span>
                            </button>
                        </div>
-                       <span className="downSpan">Minimum pallet quantity: 2</span>
+                       {/* <span className="downSpan">Minimum pallet quantity: 2</span> */}
                     </div>
                     <p className="wishlistText">
                         <i className='bx bx-star'></i>
